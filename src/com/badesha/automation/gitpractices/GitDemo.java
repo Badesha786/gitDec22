3e3a4231a0e40417f5e6c755835c9564a36703f6
Badesha786 <shaikbadesha0209@gmail.com>
2023-01-10 07:10:37 +0530

    firts commit on my framework

---
+++ src/com/badesha/automation/gitpractices/GitDemo.java
@@ -0,0 +1,10 @@
+package com.badesha.automation.gitpractices;
+
+public class GitDemo {
+public static void main(String[] args) {
+	
+	System.out.println("hello world.please do Hard work");
+	
+	
+}
+}
---
+++ src/com/badesha/automation/gitpractices/GitDemo2.java
@@ -0,0 +1,9 @@
+package com.badesha.automation.gitpractices;
+
+public class GitDemo2 {
+public static void main(String[] args) {
+	System.out.println("Hardwork=job");
+	
+	
+}
+}
